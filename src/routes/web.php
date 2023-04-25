0dd13155e15d8bd905a52518b7927537c39d2158
Susant Paudel <techbizznepal@gmail.com>
2023-04-26 04:23:43 +0545

    just practise

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Techbizz\ProductModule\ProductModuleDMLFacade;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return ProductModuleDMLFacade::getAbstractProducts();
     return view('welcome');
 });
