2831134ddba2aaddf97e068b8a59361a84a05ab3
Susant Paudel <techbizznepal@gmail.com>
2023-04-30 22:01:26 +0545

    Apply some update

--- src/config/app.php
+++ src/config/app.php
@@ -42,7 +42,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool)env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -159,7 +159,6 @@ return [
         /*
          * Package Service Providers...
          */
-
         /*
          * Application Service Providers...
          */
